3fef62db4b33ed52298e613b6ba83ef70c2a8917
Amir Ziai <arziai@gmail.com>
2021-11-29 21:57:28 -0800

    remove path append

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,12 +1,12 @@
-import os
-import sys
-
-# add path to sys
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATASETS_DIR = os.path.dirname(os.path.abspath('src/datasets'))
-sys.path.append(BASE_DIR)
-sys.path.append(DATASETS_DIR)
-
-# import datasets
-from stanford_cars import StanfordCarsDataset
-from posegen_cars import PoseGenCarsDataset
+# import os
+# import sys
+#
+# # add path to sys
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# DATASETS_DIR = os.path.dirname(os.path.abspath('src/datasets'))
+# sys.path.append(BASE_DIR)
+# sys.path.append(DATASETS_DIR)
+#
+# # import datasets
+# from stanford_cars import StanfordCarsDataset
+# from posegen_cars import PoseGenCarsDataset
